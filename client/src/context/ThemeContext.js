4b4080cdf464f47254b21c81f5753263e2a55d44
Brian Crowe <brian.j.crowe@protonmail.com>
2025-10-26 15:16:10 -0400

    use cookies to set and recall theme settings

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,39 +1,58 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
 
-const ThemeContext = createContext(null);
+const ThemeContext = createContext(null)
 
 export const ThemeProvider = (props) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [accentColor, setAccentColor] = useState('#63537d');
-  const [fontPercentage, setFontPercentage] = useState(100);
+  const cookie = Cookies.get('defaultTheme')
+  const defaultTheme = cookie
+    ? JSON.parse(cookie)
+    : { isDarkMode: false, accentColor: '#63537d', fontPercentage: 100 }
+
+  const [isDarkMode, setIsDarkMode] = useState(defaultTheme.isDarkMode)
+  const [accentColor, setAccentColor] = useState(defaultTheme.accentColor)
+  const [fontPercentage, setFontPercentage] = useState(
+    defaultTheme.fontPercentage
+  )
 
   useEffect(() => {
     if (isDarkMode) {
-      document.body.classList.add('dark');
+      document.body.classList.add('dark')
     } else {
-      document.body.classList.remove('dark');
+      document.body.classList.remove('dark')
     }
-    document.body.style.fontSize = `${fontPercentage}%`;
-  }, [isDarkMode, fontPercentage]);
+    document.body.style.fontSize = `${fontPercentage}%`
+
+    // For cookies
+    const theme = {
+      isDarkMode,
+      accentColor,
+      fontPercentage
+    }
+    // 1st time: create, update thereafter
+    Cookies.set('defaultTheme', JSON.stringify(theme))
+  }, [isDarkMode, fontPercentage, accentColor])
 
   const toggleDarkMode = () => {
-    setIsDarkMode(currentMode => !currentMode);
+    setIsDarkMode((currentMode) => !currentMode)
   }
 
   return (
-    <ThemeContext.Provider value={{
-      accentColor,
-      isDarkMode,
-      fontPercentage,
-      actions: {
-        toggleDarkMode,
-        updateAccentColor: setAccentColor,
-        updateFontPercentage: setFontPercentage
-      }
-    }}>
+    <ThemeContext.Provider
+      value={{
+        accentColor,
+        isDarkMode,
+        fontPercentage,
+        actions: {
+          toggleDarkMode,
+          updateAccentColor: setAccentColor,
+          updateFontPercentage: setFontPercentage
+        }
+      }}
+    >
       {props.children}
     </ThemeContext.Provider>
-  );
+  )
 }
 
-export default ThemeContext;
+export default ThemeContext
