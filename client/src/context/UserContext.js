2ee9e49331d60add7769bdbb4610f5f7ade9bd03
Brian Crowe <brian.j.crowe@protonmail.com>
2025-10-25 20:16:11 -0400

    user sign out works as intended

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -33,14 +33,17 @@ export const UserProvider = (props) => {
     }
   }
 
-  const signOut = () => {}
+  const signOut = () => {
+    setAuthUser(null)
+  }
 
   return (
     <UserContext.Provider
       value={{
         authUser,
         actions: {
-          signIn
+          signIn,
+          signOut
         }
       }}
     >
